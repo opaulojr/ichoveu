99664d82ed37ddf9466490aedafec4a5ad63458c
opaulojr <paulojr.hs@gmail.com>
2023-10-08 07:43:17 -0300

    implement-dark-mode

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+const themeToggleBtn = document.getElementById('theme-toggle');
+const themeIcon = document.querySelector('#theme-toggle .material-symbols-outlined.theme');
+
+const { body } = document;
+
+themeToggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+  const isDarkTheme = body.classList.contains('dark-theme');
+  themeIcon.textContent = isDarkTheme ? 'light_mode' : 'dark_mode';
+});
